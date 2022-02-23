5158a91549ddadb8f412dada786b226eaf082557
Gabriel Tauber <gabri.tauber@gmail.com>
2022-02-23 09:35:23 -0300

    New exercises and module 3

--- Module02-VueJS-and-DOM/DOM/Exercise04/app.js
+++ Module02-VueJS-and-DOM/DOM/Exercise04/app.js
@@ -6,7 +6,8 @@ new Vue({
 		ex3class: '',
 		ex4class: '',
 		ex4Value: false,
-		ex5styles: ''
+		ex5styles: '',
+		ex5Progress: '0%'
 
 	},
 	methods: {
@@ -16,7 +17,14 @@ new Vue({
 			}, 10)
 		},
 		startProgress() {
-
+			let value = 0
+			const timer = setInterval(() => {
+				value += 1
+				this.ex5Progress = `${value}%`
+				console.log(this.ex5Progress)
+				if(value == 100) clearInterval(timer)
+			}, 1)
+			
 		},
 		checkEx4Value(event) {
 			console.log(event, event.target.value)
